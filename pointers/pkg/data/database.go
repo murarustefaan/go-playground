0cd4606610dc7dc30e548d5f7cfd682987539613
Ștefan MURARU <muraru.stefaan@gmail.com>
2024-03-04 15:04:22 +0200

    feat: sync critical sections in the mocked data store

--- pointers/pkg/data/database.go
+++ pointers/pkg/data/database.go
@@ -1,21 +1,34 @@
 package data
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
-type store map[string]Item
+type skuStore map[string]Item
+type store struct {
+	data  skuStore
+	mutex sync.RWMutex
+}
 
 func (s *store) Create(item Item) (*Item, error) {
-	_, exists := (*s)[item.Name]
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	_, exists := s.data[item.Name]
 	if exists {
 		return nil, errors.New("already exists")
 	}
 
-	(*s)[item.Name] = item
+	s.data[item.Name] = item
 	return &item, nil
 }
 
 func (s *store) Read(name string) *Item {
-	item, exists := (*s)[name]
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	item, exists := s.data[name]
 	if !exists {
 		return nil
 	}
@@ -24,30 +37,39 @@ func (s *store) Read(name string) *Item {
 }
 
 func (s *store) List() []*Item {
-	items := make([]*Item, 0, len(*s))
-	for _, item := range *s {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	items := make([]*Item, 0, len(s.data))
+	for _, item := range s.data {
 		items = append(items, &item)
 	}
 	return items
 }
 
 func (s *store) Update(item Item) (*Item, error) {
-	_, exists := (*s)[item.Name]
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	_, exists := s.data[item.Name]
 	if !exists {
 		return nil, errors.New("does not exist")
 	}
 
-	(*s)[item.Name] = item
+	s.data[item.Name] = item
 	return &item, nil
 }
 
 func (s *store) Delete(name string) error {
-	_, exists := (*s)[name]
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	_, exists := s.data[name]
 	if !exists {
 		return errors.New("does not exist")
 	}
 
-	delete(*s, name)
+	delete(s.data, name)
 	return nil
 }
 
